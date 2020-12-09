a5f92c4e1b064448679d0bbfa37f5dc06f02b71d
atena-data <68478574+atena-data@users.noreply.github.com>
2020-12-08 17:46:50 -0700

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for the program.
+print("Hello there! Welcome to the band name generator :) \n")
+#2. Ask the user for their favorite color and pet name.
+favorite_color = input("What is your favorite color?\n")
+pet_name = input("What is the name of your pet?\n")
+#4. Combine the names to suggest a band name.
+print("\nYour suggested band name is " + favorite_color + " " + pet_name + "!")
+print("\nThanks for using this program!")
