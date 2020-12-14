a0d714fe16ef7a2fa13ba97fcacc087d5d5d8560
atena-data <68478574+atena-data@users.noreply.github.com>
2020-12-13 17:15:44 -0700

    Create Day 5 - PyPassword Generator.py

---
+++ Day 5 - PyPassword Generator.py
@@ -0,0 +1,33 @@
+#Password Generator Project
+import random
+#Make a list of possible numbers, letters and symbles to choose from
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ''
+for i in range (1, nr_letters+1):
+  password += random.choice(letters)
+for i in range (1, nr_symbols+1):
+  password += random.choice(symbols)
+for i in range (1, nr_numbers+1):
+  password += random.choice(numbers)
+
+print(f"Your password is: '{password}'")
+
+# Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+password_list[:0] = password
+random.shuffle(password_list)
+shuffled_password = ''
+for element in password_list:
+  shuffled_password += element
+print(f"And it can be shuffled into: '{shuffled_password}'")
