aa72146e1b7a79d2063a72bb865f67df7a96f808
atena-data <68478574+atena-data@users.noreply.github.com>
2020-12-15 18:31:12 -0700

    Update Caesar_Cipher.py

--- Day 8 - Caesar Cipher/Caesar_Cipher.py
+++ Day 8 - Caesar Cipher/Caesar_Cipher.py
@@ -1,6 +1,6 @@
 from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+print(logo)
 #takes the text and shift the letters.
 def caesar(start_text, shift_number, direction):
   new_text = ''
